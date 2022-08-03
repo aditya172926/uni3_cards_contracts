39d561f5855a400d9eb2fdf6a323d36932b421fe
Aditya <aditya26sg@gmail.com>
2022-08-03 15:08:47 +0530

    contracts deployed at mumbai

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
+require("@nomiclabs/hardhat-ethers");
+
+const { PRIVATE_KEY, API_URL } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
 };
+
+module.exports = {
+  solidity: "0.8.9",
+  defaultNetwork: "polygon_mumbai",
+  networks: {
+     hardhat: {},
+     polygon_mumbai: {
+        url: API_URL,
+        accounts: [`0x${PRIVATE_KEY}`]
+     }
+  },
+}
